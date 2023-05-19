ba0fab0e8b0bfe1c7d24e139216d59dfb0b27bc1
Dolphinator7 <petersideudogwu7@gmail.com>
2023-05-19 01:01:58 +0000

    server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Module - script that starts a Flask web application"""
 from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Handles the root url"""
-    return 'Hello HBNB!'
-
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5002)
+    app.run(host='0.0.0.0', port=5000)
+
